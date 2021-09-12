d2f9763aece27c6043ea8cdae281aae48b4a6ae8
Joshua Marangoni <joshua.marangoni@gmail.com>
2021-09-11 21:04:02 -0700

    check return code of threads

--- threads.c
+++ threads.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void* routine()
+int routine()
 {
     printf("Starting thread\n");
     sleep(3);
     printf("Ending thread\n");
-
+    return 0;
 }
 
 int main()
 {
     pthread_t t1, t2;
-    pthread_create(&t1, NULL, &routine, NULL);
-    pthread_create(&t2, NULL, &routine, NULL);
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+    if (pthread_create(&t1, NULL, &routine, NULL) !=0) return 1;
+    if (pthread_create(&t2, NULL, &routine, NULL) !=0) return 2;
+    if (pthread_join(t1, NULL) != 0) return 3;
+    if (pthread_join(t2, NULL) != 0) return 4;
     return 0;
 }
