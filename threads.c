28e16c1915652c5c486934299d1016cc9f53beaa
Joshua Marangoni <joshua.marangoni@gmail.com>
2021-09-11 20:36:36 -0700

    print from threads

--- threads.c
+++ threads.c
@@ -4,13 +4,14 @@
 
 void* routine()
 {
-    printf("Test from thread");
+    printf("Test from thread\n");
 }
 
 int main()
 {
     pthread_t t1;
     pthread_create(&t1, NULL, &routine, NULL);
+    pthread_join(t1, NULL);
     printf("Hello World\n");
     return 0;
 }
