ba297e0a47460b9103a65f73cc67cea04554b3df
Joshua Marangoni <joshua.marangoni@gmail.com>
2021-09-11 22:18:49 -0700

    spawn threads in loop

--- mutex.c
+++ mutex.c
@@ -7,7 +7,8 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#define BIG_NUM 1000000000
+#define BIG_NUM 100000000
+#define NUM_THREADS 4
 
 int mails = 0;
 pthread_mutex_t mutex;                  // declare mutex object
@@ -23,18 +24,29 @@ void *routine()
 
 int main()
 {
-    pthread_t t1, t2;
+    pthread_t th[NUM_THREADS];
 
     pthread_mutex_init(&mutex, NULL);   // initialize mutex object
 
-    if (pthread_create(&t1, NULL, &routine, NULL) !=0) return 1;
-    if (pthread_create(&t2, NULL, &routine, NULL) !=0) return 2;
-    if (pthread_join(t1, NULL) != 0) return 3;
-    if (pthread_join(t2, NULL) != 0) return 4;
+    for (int i=0; i < NUM_THREADS; i++) {
+        if (pthread_create(&th[i], NULL, &routine, NULL) !=0) {
+            printf("Failed to create thread: %d\n", i);
+            return -1;
+        }
+        printf("Thread %d has started\n", i);
+    }
+
+    for (int i=0; i < NUM_THREADS; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            printf("Failed to join thread: %d\n", i);
+            return -2;
+        }
+        printf("Thread %d has finisHed\n", i);
+    }
 
     pthread_mutex_destroy(&mutex);      // free mutex from memory
 
     printf("Number of mails: %d\n", mails);
-    printf("mails is equal correct: %d\n", mails == 2*BIG_NUM);
+    printf("mails is equal correct: %d\n", mails == NUM_THREADS*BIG_NUM);
     return 0;
 }
